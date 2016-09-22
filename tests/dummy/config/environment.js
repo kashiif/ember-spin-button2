2af5effb3f160456778d9d98738f6c086ac49556
Kashif Khan <kkhan@multiservice.com>
2016-09-22 23:50:38 +1000

    One more attempt to fix baseUrl

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -40,7 +40,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = '/demo/';
+    ENV.baseURL = '/ember-spin-button2/demo/';
   }
 
   return ENV;
