78de07269efd15cba1e0e61f52cdc002de3a4c6a
Kashif Khan <kkhan@multiservice.com>
2016-09-25 19:31:08 +1000

    Update dummy app

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,25 +1,109 @@
 import Ember from 'ember';
 const { String: { w } } = Ember;
+
+function getAttributesForGeneratedCode() {
+  let selectedColor = this.get('selectedColorForSpinButton'),
+    buttonStyle = this.get('selectedAnimation'),
+    cssClass = this.get('classNamesForSpinButton');
+
+  if (selectedColor) {
+    selectedColor = `  data-color="${selectedColor}"`;
+  }
+
+  if (cssClass) {
+    cssClass = `  class="${cssClass}"`;
+  }
+
+  if (buttonStyle) {
+    buttonStyle = `  buttonStyle="${buttonStyle}"`;
+  }
+
+  return [
+    '  action=(action "myCustomAction")',
+    selectedColor,
+    cssClass,
+    buttonStyle
+  ];
+}
+
 export default Ember.Controller.extend({
-  inFlight: false,
+  booleanControllerVarForSpinner: false,
 
   buttonStyles: w('expand-left expand-right expand-up expand-down contract contract-overlay zoom-in zoom-out slide-left slide-right slide-up slide-down'),
   buttonColors: w('green red blue mint purple'),
 
+  buttonClassNames: 'btn btn-primary',
+  buttonLabel: 'Submit',
+  colorPreference: 'css-class',
   selectedAnimation: 'expand-left',
-  templateCode: Ember.computed('selectedColor', 'selectedAnimation', 'timeout', 'inFlight', function(){
+  selectedColor: 'green',
 
-    return '{{#spin-button\n' +
-      (this.get('selectedColor') ? '\tdata-color=' + this.get('selectedColor') + '\n' : '') +
-      //'\tinFlight=inFlight'  + '\n' +
-      '\taction=(action "myCustomAction")'  + '\n' +
-      '\tdefaultTimeout=' + this.get('timeout')  + '\n' +
-      (this.get('selectedAnimation') ? '\tbuttonStyle="' + this.get('selectedAnimation') + '"\n' : '') +
-      '}} Do Something Expensive' + '\n' +
-    '{{/spin-button}}';
+  templateCodeNonPromise: Ember.computed('selectedColorForSpinButton', 'selectedAnimation', 'timeout', 'inFlight', 'classNames', 'buttonLabel',
+    function(){
+
+      let timeout = this.get('timeout'),
+          buttonLabel= this.get('buttonLabel'),
+          attributes = getAttributesForGeneratedCode.call(this);
+
+
+      let lines = ['{{#spin-button',
+        '  inFlight=booleanControllerVarForSpinner',
+        `  defaultTimeout=${timeout}`]
+        .concat(attributes)
+        .concat([
+          '}}',
+          `  ${buttonLabel}`,
+          '{{/spin-button}}'
+        ]);
+
+      return lines.filter((item) => item).join('\n');
 
   }),
+
+  templateCodeWithPromise: Ember.computed('selectedColorForSpinButton', 'selectedAnimation', 'timeout', 'inFlight', 'classNames', 'buttonLabel',
+    function(){
+
+      let buttonLabel= this.get('buttonLabel'),
+          attributes = getAttributesForGeneratedCode.call(this);
+
+      let lines = ['{{#spin-button']
+          .concat(attributes)
+          .concat([
+            '}}',
+            `  ${buttonLabel}`,
+            '{{/spin-button}}'
+          ]);
+
+      return lines.filter((item) => item).join('\n');
+
+  }),
+
   timeout: 2E3,
+
+  isTimeoutEnabled: Ember.computed('timeout', function(){
+    let t = this.get('timeout') | 0;
+    return t > 4;
+  }),
+
+  selectedColorForSpinButton: Ember.computed('selectedColor', 'colorPreference', function(){
+    let pref = this.get('colorPreference');
+
+    if (pref === 'built-in') {
+      return this.get('selectedColor');
+    }
+
+    return '';
+  }),
+
+  classNamesForSpinButton: Ember.computed('buttonClassNames', 'colorPreference', function(){
+    let pref = this.get('colorPreference');
+
+    if (pref === 'css-class') {
+      return this.get('buttonClassNames');
+    }
+
+    return '';
+  }),
 
   actions: {
     createWithPromise: function () {
